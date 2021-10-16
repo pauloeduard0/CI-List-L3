7baecc4b07f9dfaebaccf0b4332bfd1d581c8a62
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-10-16 10:15:44 -0300

    Lista de Plataformas e de Publicadoras

---
+++ Platform.py
@@ -0,0 +1,5 @@
+def platform(result):
+    p = ['PS4', 'PC', 'XB', 'Wii', 'GB', 'GBA', 'X360', 'PS3', 'PS2']
+    for plat in p:
+        if plat == result:
+            return plat
---
+++ Publishers.py
@@ -0,0 +1,6 @@
+def publisher(result):
+    p = ['Nintendo', 'MicrosoftGameStudios', 'TakeTwoInteractive',
+         'SonyComputerEntertainment', 'Activision', 'SquareSoft', 'BethesdaSoftworks', 'ElectronicArts']
+    for publi in p:
+        if publi == result:
+            return publi
