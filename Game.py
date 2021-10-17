a1615ae1a9587104dd471018c1c3aff8b0c71512
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-10-17 12:17:34 -0300

    Interface transferida para um novo arquivo

---
+++ Game.py
@@ -0,0 +1,11 @@
+from ServiceGame import escolher
+
+
+class Game:
+    escolha = input('P1 para Plataforma ou P2 para Publisher: ')
+    if escolha == 'P1':
+        tipo = input('Digite a plataforma desejada: ')
+        escolher(escolha, tipo)
+    elif escolha == 'P2':
+        tipo = input('Digite a publicadora desejada: ')
+        escolher(escolha, tipo)
